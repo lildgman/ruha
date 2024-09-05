d79e5ffde2ea26292cb027b3e9d415860bb85a6e
lildgman <humanorc2357@gmail.com>
2024-09-05 14:54:01 +0900

    create project

---
+++ src/main/java/com/ruha/RuhaApplication.java
@@ -0,0 +1,13 @@
+package com.ruha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruha/RuhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
