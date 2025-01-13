6d6ba2844c913a5b3bd9573fdfafd9183e7c7119
lildgman <humanorc2357@gmail.com>
2025-01-13 18:40:23 +0900

    update build.gradle

--- src/main/java/com/ruha/RuhaApplication.java
+++ src/main/java/com/ruha/RuhaApplication.java
@@ -1,11 +1,9 @@
 package com.ruha;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.ruha.mapper")
 public class RuhaApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/ruha/dto/InsertMemberDto.java
@@ -0,0 +1,12 @@
+package com.ruha.dto;
+
+import lombok.Getter;
+
+
+@Getter
+public class InsertMemberDto {
+
+    private String email;
+    private String password;
+    private String name;
+}
